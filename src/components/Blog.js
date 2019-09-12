042fcc6b6f137ac1c2d552603501891d674fee5d
Moses Holmström <thykka@gmail.com>
2019-09-12 15:24:13 +0300

    5.6 - blogilistan frontend, step6

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,10 +1,25 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    <a
-      href={blog.url}
-      title={'Posted by ' + blog.user.name}>{blog.title}</a> - {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [blogExpanded, setBlogExpanded] = useState(false);
+
+  return (
+    <div className={'blog' + (blogExpanded ? ' expanded' : '') }>
+      <button
+        onClick={() => setBlogExpanded(!blogExpanded)}
+        >
+        { blogExpanded ? '-' : '+' }
+      </button>
+      <a
+        href={blog.url}
+        >
+        {blog.title}
+      </a>
+      <div className="details" style={{display: !blogExpanded ? 'none' : ''}}>
+        Author: {blog.author}<br/>
+        Posted by: {blog.user.name}
+      </div>
+    </div>
+  );
+};
 
 export default Blog
